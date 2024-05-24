d6841524456f839229c648075a5044b8c17c606d
anushamittal1 <112055083+anushamittal1@users.noreply.github.com>
2024-05-24 00:15:04 -0500

    Add files via upload

---
+++ script1.sql
@@ -0,0 +1,7 @@
+USE project_db;
+INSERT INTO Users (UserName, User_Email, UserAge) VALUES
+('Alice', 'alice@example.com', 25),
+('Bob', 'bob@example.com', 30),
+('Charlie', 'charlie@example.com', 35);
+
+SELECT * FROM Users;
